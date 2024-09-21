6d06f7cb41254613f754c38443370402cbc427bc
MonifaAnna <monifamajed21@gmail.com>
2024-09-22 00:04:21 +0600

    add money done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,21 @@
 
+
+// step-1
+document.getElementById('add-moneyBtn').addEventListener('click', function (event) {
+    event.preventDefault();
+    // step-2
+    const pinNumber = document.getElementById('pin').value;
+    const addMoney = document.getElementById('money-add').value;
+    const convertAddMoney = parseInt(addMoney);
+    const mainBalance = document.getElementById('main-balance');
+    const convertMainBalance = parseInt(mainBalance.innerText);
+    const pin = '2021';
+
+    if (pinNumber === pin) {
+        const currentBalance = convertAddMoney + convertMainBalance;
+        mainBalance.innerText = currentBalance;
+    } else {
+        alert('wrong info')
+    }
+    
+});
